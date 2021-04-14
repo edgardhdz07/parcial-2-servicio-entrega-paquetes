869b8d9a43f85e71d9d349a041d806f693af6d70
Edgar David Hernández <edgardavidhdz07@gmail.com>
2021-04-14 15:06:44 -0500

    Create clients.py

---
+++ models/clients.py
@@ -0,0 +1,16 @@
+Clients = [
+    {
+        "id": 8,
+        "name": "Pedro",
+        "Last_name": "Garcia Perez",
+        "address": "Calle 8 #20-50",
+        "postal_code": 130010
+    },
+    {
+        "id": 9,
+        "name": "Camilo",
+        "Last_name": "Lora Contreras",
+        "address": "Calle 60 Kr 17",
+        "postal_code": 130002
+    }
+]
